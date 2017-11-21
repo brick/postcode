301857e0389eaa44d48617850225174387cd5862
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-22 00:33:40 +0100

    Use constant for pattern

--- src/Formatter/GB.php
+++ src/Formatter/GB.php
@@ -26,35 +26,25 @@ class GB implements CountryPostcodeFormatter
 {
     /**
      * The regular expression pattern used to validate the postcode and extract the outcode and incode.
-     *
-     * @var string
      */
-    private $pattern;
-
-    /**
-     * Class constructor.
-     */
-    public function __construct()
-    {
-        $this->pattern = '/^'
-            . '('
-            . '(?:[A-Z][0-9])|'
-            . '(?:[A-Z][0-9][A-Z])|'
-            . '(?:[A-Z][0-9][0-9])|'
-            . '(?:[A-Z][A-Z][0-9])|'
-            . '(?:[A-Z][A-Z][0-9][A-Z])|'
-            . '(?:[A-Z][A-Z][0-9][0-9])'
-            . ')'
-            . '([0-9][A-Z][A-Z])'
-            . '$/';
-    }
+    private const PATTERN = '/^'
+        . '('
+        . '(?:[A-Z][0-9])|'
+        . '(?:[A-Z][0-9][A-Z])|'
+        . '(?:[A-Z][0-9][0-9])|'
+        . '(?:[A-Z][A-Z][0-9])|'
+        . '(?:[A-Z][A-Z][0-9][A-Z])|'
+        . '(?:[A-Z][A-Z][0-9][0-9])'
+        . ')'
+        . '([0-9][A-Z][A-Z])'
+        . '$/';
 
     /**
      * {@inheritdoc}
      */
     public function format(string $postcode) : ?string
     {
-        if (preg_match($this->pattern, strtoupper($postcode), $matches) !== 1) {
+        if (preg_match(self::PATTERN, strtoupper($postcode), $matches) !== 1) {
             return null;
         }
 
