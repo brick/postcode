e5016103697f8caaff9ba8fbbf3dca067e0f4a2a
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-23 22:19:22 +0100

    Formatter for AR (Argentina)

---
+++ src/Formatter/ARFormatter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Argentina.
+ *
+ * The postcode is either 4 digits, or 1 letter + 4 digits + 3 letters, with no separators.
+ *
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Argentina
+ */
+class ARFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (preg_match('/^(([0-9]{4})|([A-Z][0-9]{4}[A-Z]{3}))$/', $postcode) !== 1) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/ARTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\ARFormatter;
+use Brick\Postcode\Tests\CountryTest;
+
+/**
+ * Unit tests for the AR postcode formatter.
+ */
+class ARTest extends CountryTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormatter() : CountryPostcodeFormatter
+    {
+        return new ARFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerPostcode() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', '1234'],
+            ['12345', null],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+
+            ['A1234', null],
+            ['A1234B', null],
+            ['A1234BC', null],
+            ['A1234BCD', 'A1234BCD'],
+            ['A1234BCDE', null],
+            ['A12345BC', null],
+            ['A12345BCD', null],
+        ];
+    }
+}
