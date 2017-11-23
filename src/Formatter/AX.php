344cb419311b478fc601b1b7d8200d8d56cb6660
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-23 13:09:07 +0100

    Formatter for AX (Åland Islands)

---
+++ src/Formatter/AX.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Åland Islands.
+ *
+ * Postcodes consist of 5 digits, starting with 22.
+ * Postcodes may optionally start with "AX-" when used from abroad.
+ */
+class AX implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        $length = strlen($postcode);
+        $prefix = false;
+
+        if ($length === 7) {
+            if (substr($postcode, 0, 2) !== 'AX') {
+                return null;
+            }
+
+            $postcode = substr($postcode, 2);
+            $prefix = true;
+        } elseif (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if (substr($postcode, 0, 2) !== '22') {
+            return null;
+        }
+
+        if ($prefix) {
+            return 'AX-' . $postcode;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/AXTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\AX;
+use Brick\Postcode\Tests\CountryTest;
+
+/**
+ * Unit tests for the AX postcode formatter.
+ */
+class AXTest extends CountryTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormatter() : CountryPostcodeFormatter
+    {
+        return new AX();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerPostcode() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['123456', null],
+
+            ['AB22222', null],
+            ['AX22222', 'AX-22222'],
+
+            ['22345', '22345'],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null]
+        ];
+    }
+}
