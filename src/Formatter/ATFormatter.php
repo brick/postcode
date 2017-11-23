4c5c55cd8b9c25e89e3fc6d29e42760568818ce7
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-23 22:29:47 +0100

    Formatter for AT (Austria)

---
+++ src/Formatter/ATFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Austria.
+ *
+ * Postcodes consist of 4 digits, without separator.
+ *
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Austria
+ */
+class ATFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if (strlen($postcode) !== 4) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/ATTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\ATFormatter;
+use Brick\Postcode\Tests\CountryTest;
+
+/**
+ * Unit tests for the AT postcode formatter.
+ */
+class ATTest extends CountryTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormatter() : CountryPostcodeFormatter
+    {
+        return new ATFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerPostcode() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', '1234'],
+            ['12345', null],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null]
+        ];
+    }
+}
