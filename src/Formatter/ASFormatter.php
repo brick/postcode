dfbb795396b7607311e7f7292d1abe2d8726b07c
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-23 18:08:06 +0100

    Formatter for AS (American Samoa)

---
+++ src/Formatter/ASFormatter.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in American Samoa.
+ *
+ * Mail service in American Samoa is fully integrated with the United States Postal Service.
+ * All points in the territory use the same ZIP code, 96799.
+ *
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_American_Samoa
+ */
+class ASFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        $length = strlen($postcode);
+
+        if ($length === 5) {
+            if ($postcode !== '96799') {
+                return null;
+            }
+
+            return $postcode;
+        }
+
+        if ($length !== 9) {
+            return null;
+        }
+
+        $zip = substr($postcode, 0, 5);
+
+        if ($zip !== '96799') {
+            return null;
+        }
+
+        $plusFour = substr($postcode, 5);
+
+        return $zip . '-' . $plusFour;
+    }
+}
---
+++ tests/Formatter/ASTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\ASFormatter;
+use Brick\Postcode\Tests\CountryTest;
+
+/**
+ * Unit tests for the AS postcode formatter.
+ */
+class ASTest extends CountryTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormatter() : CountryPostcodeFormatter
+    {
+        return new ASFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerPostcode() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['12345', null],
+            ['96799', '96799'],
+            ['123456', null],
+            ['1234567', null],
+            ['12345678', null],
+            ['123456789', null],
+            ['967991234', '96799-1234'],
+            ['1234567890', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+            ['ABCDEFG', null],
+            ['ABCDEFGH', null],
+            ['ABCDEFGHI', null],
+            ['ABCDEFGHIJK', null]
+        ];
+    }
+}
