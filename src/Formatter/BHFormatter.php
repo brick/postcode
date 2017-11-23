5198f718fa708f1f910988c14d322972271712aa
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-23 23:14:21 +0100

    Formatter for BH (Bahrain)

---
+++ src/Formatter/BHFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Bahrain.
+ *
+ * Valid post code numbers are 101 to 1216 with gaps in the range. Known as block number formally.
+ * The first 1 or 2 digits refer to one of the 12 municipalities of the country.
+ */
+class BHFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (preg_match('/^(1?[0-9])([0-9]{2})$/', $postcode, $matches) !== 1) {
+            return null;
+        }
+
+        $municipality = (int) $matches[1];
+
+        if ($municipality < 1 || $municipality > 12) {
+            return null;
+        }
+
+        if ($matches[2] === '00') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/BHTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\BHFormatter;
+use Brick\Postcode\Tests\CountryTest;
+
+/**
+ * Unit tests for the BH postcode formatter.
+ */
+class BHTest extends CountryTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormatter() : CountryPostcodeFormatter
+    {
+        return new BHFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerPostcode() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['12345', null],
+            ['123456', null],
+
+            ['000', null],
+            ['001', null],
+            ['100', null],
+            ['101', '101'],
+
+            ['0123', null],
+            ['1000', null],
+            ['1001', '1001'],
+            ['1301', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null]
+        ];
+    }
+}
