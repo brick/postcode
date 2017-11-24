04b5ce36968e14cc1525ae1e86d3e051c2944a2b
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-24 13:33:18 +0100

    Formatter for VG (British Virgin Islands)

---
+++ src/Formatter/VGFormatter.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in British Virgin Islands.
+ *
+ * The postcode format is VG followed by 4 digits, specifically VG1110 through VG1160.
+ * This formatter accepts inputs with and without the VG prefix, and always outputs the prefix.
+ */
+class VGFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (substr($postcode, 0, 2) === 'VG') {
+            $postcode = substr($postcode, 2);
+        }
+
+        if (strlen($postcode) !== 4) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if ($postcode < '1110' || $postcode > '1160') {
+            return null;
+        }
+
+        return 'VG' . $postcode;
+    }
+}
---
+++ tests/Formatter/VGFormatterTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\VGFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the VG postcode formatter.
+ */
+class VGFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new VGFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1109', null],
+            ['1110', 'VG1110'],
+            ['1111', 'VG1111'],
+            ['1159', 'VG1159'],
+            ['1160', 'VG1160'],
+            ['1161', null],
+            ['12345', null],
+
+            ['A', null],
+            ['AB', null],
+            ['AB1110', null],
+            ['AVG1110', null],
+            ['VG01110', null],
+            ['VG11100', null],
+
+            ['VG', null],
+            ['VG1', null],
+            ['VG12', null],
+            ['VG123', null],
+            ['VG1109', null],
+            ['VG1110', 'VG1110'],
+            ['VG1111', 'VG1111'],
+            ['VG1159', 'VG1159'],
+            ['VG1160', 'VG1160'],
+            ['VG1161', null],
+            ['AB123', null],
+            ['VGZ12', null],
+            ['A1234', null],
+        ];
+    }
+}
