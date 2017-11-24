5c86be41225a953c6eb0f4a57a4746710ff0c46e
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-24 20:42:10 +0100

    Formatter for CN (China)

---
+++ src/Formatter/CNFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in China.
+ *
+ * China Post uses a six-digit all-numerical system, without separator.
+ * The range 000000–009999 is not in use.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes_in_China
+ */
+class CNFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if (strlen($postcode) !== 6) {
+            return null;
+        }
+
+        if ($postcode[0] === '0' && $postcode[1] === '0') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/CNFormatterTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\CNFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the CN postcode formatter.
+ */
+class CNFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new CNFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['000000', null],
+            ['010000', '010000'],
+            ['100000', '100000'],
+            ['123456', '123456'],
+            ['12345X', null],
+            ['X12345', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+        ];
+    }
+}
