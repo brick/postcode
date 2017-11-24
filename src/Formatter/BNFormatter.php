1aefbd2f013c7980b9a5a7fb02249db726799ac9
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-24 13:39:05 +0100

    Formatter for BN (Brunei)

---
+++ src/Formatter/BNFormatter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Brunei.
+ *
+ * Postcode format is two letters followed by 4 digits, without separator.
+ *
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Brunei
+ */
+class BNFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (preg_match('/^[A-Z]{2}[0-9]{4}$/', $postcode, $matches) !== 1) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/BNFormatterTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\BNFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the BN postcode formatter.
+ */
+class BNFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new BNFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+
+            ['A12345', null],
+            ['ABC123', null],
+
+            ['AB1234', 'AB1234'],
+            ['XX9999', 'XX9999'],
+        ];
+    }
+}
