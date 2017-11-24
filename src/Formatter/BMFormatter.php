a47656aae572a5b31e4cd55014a30f06f29c8649
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-24 10:31:07 +0100

    Formatter for BM (Bermuda)

---
+++ src/Formatter/BMFormatter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Bermuda.
+ *
+ * Postcode formats are AA NN for street addresses, AA AA for P.O. Box addresses (A=letter, N=digit).
+ *
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Bermuda
+ */
+class BMFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (preg_match('/^([A-Z]{2})([A-Z]{2}|[0-9]{2})$/', $postcode, $matches) !== 1) {
+            return null;
+        }
+
+        return $matches[1] . ' ' . $matches[2];
+    }
+}
---
+++ tests/Formatter/BMFormatterTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\BMFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the BM postcode formatter.
+ */
+class BMFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new BMFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+
+            ['A', null],
+            ['A1', null],
+            ['A12', null],
+            ['A123', null],
+            ['AB', null],
+            ['AB1', null],
+            ['AB12', 'AB 12'],
+            ['AB123', null],
+            ['ABC', null],
+            ['ABC1', null],
+            ['ABC12', null],
+            ['ABCD', 'AB CD'],
+            ['ABCDE', null],
+            ['ABCD1', null],
+            ['1ABC', null],
+            ['1ABCD', null],
+            ['1AB23', null],
+        ];
+    }
+}
