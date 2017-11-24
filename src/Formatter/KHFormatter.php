2f4be20601aac29f1a82776551e64560cd61bd00
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-24 13:43:11 +0100

    Formatter for KH (Cambodia)

---
+++ src/Formatter/KHFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Cambodia.
+ *
+ * Postcodes consist of 5 digits, without separator.
+ */
+class KHFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
