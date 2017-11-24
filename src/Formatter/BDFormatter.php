6200e2940c1aa677d1a5d35fcee464ecddb283c3
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-24 08:32:24 +0100

    Formatter for BD (Bangladesh)

---
+++ src/Formatter/BDFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Bangladesh.
+ *
+ * Postcodes consist of 4 digits, without separator.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes_in_Bangladesh
+ */
+class BDFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if (strlen($postcode) !== 4) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
