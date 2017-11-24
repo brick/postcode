3696715b524d285138b2c2cbeb8d96d707be5ead
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-24 20:19:51 +0100

    Formatter for KY (Cayman Islands)

---
+++ src/Formatter/KYFormatter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in the Cayman Islands.
+ *
+ * Postcode format is KYN-NNNN, where N are digits. The first digit can only be 1 to 3.
+ *
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_the_Cayman_Islands
+ */
+class KYFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (substr($postcode, 0, 2) !== 'KY') {
+            return null;
+        }
+
+        $postcode = substr($postcode, 2);
+
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if ($postcode[0] < '1' || $postcode[0] > '3') {
+            return null;
+        }
+
+        return 'KY' . $postcode[0] . '-' . substr($postcode, 1);
+    }
+}
---
+++ tests/Formatter/KYFormatterTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\KYFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the KY postcode formatter.
+ */
+class KYFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new KYFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+
+            ['KY01234', null],
+            ['KY12345', 'KY1-2345'],
+            ['KY23456', 'KY2-3456'],
+            ['KY34567', 'KY3-4567'],
+            ['KY45678', null],
+            ['KX12345', null],
+            ['XY12345', null],
+            ['KKY1234', null],
+            ['KKY12345', null],
+            ['KY1234', null],
+            ['KY123456', null],
+        ];
+    }
+}
