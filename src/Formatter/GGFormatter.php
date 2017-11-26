1e8b1b79d26cf37695b9e41808cad02d05a9ecb4
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-26 17:37:04 +0100

    Formatter for GG (Guernsey)

---
+++ src/Formatter/GGFormatter.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats the postcodes in Guernsey.
+ *
+ * Postcodes can have two different formats:
+ *
+ * - AAN NAA
+ * - AANN NAA
+ *
+ * A stands for a capital letter, N stands for a digit.
+ * The first two letters are always GY.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/GY_postcode_area
+ */
+class GGFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (preg_match('/^(GY[0-9]{1,2})([0-9][A-Z][A-Z])$/', strtoupper($postcode), $matches) !== 1) {
+            return null;
+        }
+
+        return $matches[1] . ' ' . $matches[2];
+    }
+}
---
+++ tests/Formatter/GGFormatterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\GGFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the GG postcode formatter.
+ */
+class GGFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new GGFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['AA999AA', null],
+            ['XY999AA', null],
+            ['GX999AA', null],
+            ['GY999AA', 'GY99 9AA'],
+            ['GY123AB', 'GY12 3AB'],
+            ['XGY123AB', null],
+            ['GY123ABC', null],
+            ['GY9999A', null],
+            ['GY999A9', null],
+        ];
+    }
+}
