0e4cd3b06dd0429d7ee43f9577f8acbe58f7129d
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-26 13:28:38 +0100

    Formatter for CC (Cocos (Keeling) Island)

---
+++ src/Formatter/CCFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Cocos (Keeling) Island.
+ *
+ * Postcodes consist of 4 digits, without separator.
+ */
+class CCFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 4) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
