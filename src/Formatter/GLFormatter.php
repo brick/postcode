b0c1a40495526736e244649c75f777b73ebc86dc
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-26 17:13:42 +0100

    Formatter for GL (Greenland)

---
+++ src/Formatter/GLFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Greenland.
+ *
+ * Postcodes consist of 4 digits, without separator.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ */
+class GLFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 4) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
