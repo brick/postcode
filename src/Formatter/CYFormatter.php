464382d4e20ff6c1f67e3af49da4c6bbfe85dce5
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-26 15:30:18 +0100

    Formatter for CY (Cyprus)

---
+++ src/Formatter/CYFormatter.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Cyprus.
+ *
+ * Postcodes consist of 4 digits, without separator.
+ * The postal code system covers the whole island, but is not used on mail to Northern Cyprus.
+ * Northern Cyprus uses a 5-digit code commencing 99, introduced in 2013.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Cyprus
+ */
+class CYFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        $length = strlen($postcode);
+
+        if ($length === 4) {
+            return $postcode;
+        }
+
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (substr($postcode, 0, 2) !== '99') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/CYFormatterTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\CYFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the CY postcode formatter.
+ */
+class CYFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new CYFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['0000', '0000'],
+            ['1234', '1234'],
+            ['9999', '9999'],
+            ['00000', null],
+            ['12345', null],
+            ['98999', null],
+            ['99000', '99000'],
+            ['99999', '99999'],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+        ];
+    }
+}
