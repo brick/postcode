081c88b5d247f20e11eaae879f28540592ccabda
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-26 17:16:39 +0100

    Formatter for GP (Guadeloupe)

---
+++ src/Formatter/GPFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Guadeloupe.
+ *
+ * Postcode format is 971NN, where N stands for a digit.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ */
+class GPFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (substr($postcode, 0, 3) !== '971') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/GPFormatterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\GPFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the GP postcode formatter.
+ */
+class GPFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new GPFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['97099', null],
+            ['97100', '97100'],
+            ['97199', '97199'],
+            ['97200', null],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+        ];
+    }
+}
