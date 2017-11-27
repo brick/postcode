ad9d76b918cb9c271ec8017837112d719cf8dbb1
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-27 11:46:42 +0100

    Formatter for IS (Iceland)

---
+++ src/Formatter/ISFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Iceland.
+ *
+ * Postcodes consist of 3 digits, without separator.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes_in_Iceland
+ */
+class ISFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 3) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if ($postcode[0] === '0') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/ISFormatterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\ISFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the IS postcode formatter.
+ */
+class ISFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new ISFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['012', null],
+            ['123', '123'],
+            ['999', '999'],
+            ['1234', null],
+            ['12345', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+        ];
+    }
+}
