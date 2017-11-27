2bf0ff5168177c926a6d1b2774a6f588f67a9480
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-27 11:54:43 +0100

    Formatter for ID (Indonesia)

---
+++ src/Formatter/IDFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Indonesia.
+ *
+ * Postcodes consist of 5 digits, without separator.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Indonesia
+ */
+class IDFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if ($postcode[0] === '0') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/IDFormatterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\IDFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the ID postcode formatter.
+ */
+class IDFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new IDFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['012', null],
+            ['123', null],
+            ['1234', null],
+            ['01234', null],
+            ['12345', '12345'],
+            ['99999', '99999'],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+        ];
+    }
+}
