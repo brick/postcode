cd1e4f59ee3851fd1164e6b6bcfc530dcadc7c82
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-28 15:43:43 +0100

    Formatter for IL (Israel)

---
+++ src/Formatter/ILFormatter.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Israel.
+ *
+ * Postcodes consist of 7 digits, without separator.
+ * The country used 5 digit postcodes up to 2013. These postcodes are not supported by this formatter.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Israel
+ */
+class ILFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 7) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
