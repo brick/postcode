4622e1469fba692b7fa940ca28793687b2639cd7
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-29 11:30:02 +0100

    Formatter for KZ (Kazakhstan)

---
+++ src/Formatter/KZFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Kazakhstan.
+ *
+ * Postal codes in Kazakhstan are 6 digit numeric.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ */
+class KZFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 6) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
