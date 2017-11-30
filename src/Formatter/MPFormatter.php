a75ffa002b3511c850f477232d633d4b68190d09
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-30 20:08:33 +0100

    Formatter for MP (Northern Mariana Islands)

---
+++ src/Formatter/MPFormatter.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Northern Mariana Islands.
+ *
+ * U.S. ZIP codes. Range 96950 - 96952.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_the_Northern_Mariana_Islands
+ */
+class MPFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        $length = strlen($postcode);
+
+        if ($length === 5) {
+            $zip = $postcode;
+        } elseif ($length === 9) {
+            $zip = substr($postcode, 0, 5);
+        } else {
+            return null;
+        }
+
+        if ($zip < '96950' || $zip > '96952') {
+            return null;
+        }
+
+        if ($length === 5) {
+            return $postcode;
+        }
+
+        return $zip . '-' . substr($postcode, 5);
+    }
+}
---
+++ tests/Formatter/MPFormatterTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\MPFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the MP postcode formatter.
+ */
+class MPFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new MPFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['12345', null],
+            ['96949', null],
+            ['96950', '96950'],
+            ['96951', '96951'],
+            ['96952', '96952'],
+            ['96953', null],
+            ['123456', null],
+            ['1234567', null],
+            ['12345678', null],
+            ['123456789', null],
+            ['969490000', null],
+            ['969501234', '96950-1234'],
+            ['969529999', '96952-9999'],
+            ['969530000', null],
+            ['1234567890', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+            ['ABCDEFG', null],
+            ['ABCDEFGH', null],
+            ['ABCDEFGHI', null],
+            ['ABCDEFGHIJK', null],
+        ];
+    }
+}
