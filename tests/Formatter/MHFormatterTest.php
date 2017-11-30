14a0e6ab77176c952cb358c320975f3a27b767bc
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-30 19:31:23 +0100

    Formatter for MH (Marshall Islands)

---
+++ tests/Formatter/MHFormatterTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\MHFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the MH postcode formatter.
+ */
+class MHFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new MHFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['12345', null],
+            ['96959', null],
+            ['96960', '96960'],
+            ['96965', '96965'],
+            ['96970', '96970'],
+            ['96971', null],
+            ['123456', null],
+            ['1234567', null],
+            ['12345678', null],
+            ['123456789', null],
+            ['969590000', null],
+            ['969601234', '96960-1234'],
+            ['969709999', '96970-9999'],
+            ['969710000', null],
+            ['1234567890', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+            ['ABCDEFG', null],
+            ['ABCDEFGH', null],
+            ['ABCDEFGHI', null],
+            ['ABCDEFGHIJK', null],
+        ];
+    }
+}
