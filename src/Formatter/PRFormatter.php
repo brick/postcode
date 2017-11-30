000dd530ef64e428e0682f11a5acfdd282cfcc3f
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-30 21:23:41 +0100

    Formatter for PR (Puerto Rico)

---
+++ src/Formatter/PRFormatter.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Puerto Rico.
+ *
+ * Puerto Rico is allocated the US ZIP codes 00600 to 00799 and 00900 to 00999.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Puerto_Rico
+ */
+class PRFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        $length = strlen($postcode);
+
+        if ($length === 5) {
+            $zip = $postcode;
+        } elseif ($length === 9) {
+            $zip = substr($postcode, 0, 5);
+        } else {
+            return null;
+        }
+
+        if (! $this->isInRange($zip)) {
+            return null;
+        }
+
+        if ($length === 5) {
+            return $postcode;
+        }
+
+        return $zip . '-' . substr($postcode, 5);
+    }
+
+    /**
+     * @param string $zip
+     *
+     * @return bool
+     */
+    private function isInRange(string $zip) : bool
+    {
+        if ($zip >= '00600' && $zip <= '00799') {
+            return true;
+        }
+
+        if ($zip >= '00900' && $zip <= '00999') {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ tests/Formatter/PRFormatterTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\PRFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the PR postcode formatter.
+ */
+class PRFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new PRFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['00599', null],
+            ['00600', '00600'],
+            ['00799', '00799'],
+            ['00800', null],
+            ['00899', null],
+            ['00900', '00900'],
+            ['00999', '00999'],
+            ['01000', null],
+
+            ['000600', null],
+            ['006000', null],
+
+            ['005991234', null],
+            ['006001234', '00600-1234'],
+            ['007991234', '00799-1234'],
+            ['008001234', null],
+            ['008991234', null],
+            ['009001234', '00900-1234'],
+            ['009991234', '00999-1234'],
+            ['010001234', null],
+
+            ['06001234', null],
+            ['00600123', null],
+
+            ['0006001234', null],
+            ['0060012340', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['123456', null],
+            ['1234567', null],
+            ['12345678', null],
+            ['123456789', null],
+            ['1234567890', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+            ['ABCDEFG', null],
+            ['ABCDEFGH', null],
+            ['ABCDEFGHI', null],
+            ['ABCDEFGHIJ', null],
+        ];
+    }
+}
