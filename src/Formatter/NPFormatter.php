cc308dfe6f5929af65b7342810c6aaa0b835fe47
Benjamin Morel <benjamin.morel@gmail.com>
2017-12-01 11:25:12 +0100

    Formatter for NP (Nepal)

---
+++ src/Formatter/NPFormatter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Nepal.
+ *
+ * Postcodes consist of 5 digits, without separator.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes_in_Nepal
+ */
+class NPFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
