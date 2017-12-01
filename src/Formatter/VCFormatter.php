7ec216579106c557f7a1f1dff011db3d256c0068
Benjamin Morel <benjamin.morel@gmail.com>
2017-12-01 15:14:26 +0100

    Formatter for VC (Saint Vincent and the Grenadines)

---
+++ src/Formatter/VCFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Saint Vincent and the Grenadines.
+ *
+ * The postcode format is VCNNNN, where N represents a digit.
+ * This formatter accepts postcodes with and without the VC prefix, and always outputs the prefix.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ */
+class VCFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (substr($postcode, 0, 2) === 'VC') {
+            $postcode = substr($postcode, 2);
+        }
+
+        if (strlen($postcode) !== 4) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return 'VC' . $postcode;
+    }
+}
---
+++ tests/Formatter/VCFormatterTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\VCFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the VC postcode formatter.
+ */
+class VCFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new VCFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', 'VC1234'],
+            ['12345', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+
+            ['VC', null],
+            ['VC1', null],
+            ['VC123', null],
+            ['VC1234', 'VC1234'],
+            ['VX1234', null],
+            ['XC1234', null],
+            ['VC12345', null],
+            ['XVC1234', null],
+        ];
+    }
+}
