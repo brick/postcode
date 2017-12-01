6ac3a19f0291207d23cfbb6b42249c6e89ffa183
Benjamin Morel <benjamin.morel@gmail.com>
2017-12-01 13:55:29 +0100

    Formatter for RO (Romania)

---
+++ src/Formatter/ROFormatter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Romania.
+ *
+ * Postal codes have six digits and no separator.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Romania
+ */
+class ROFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 6) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
