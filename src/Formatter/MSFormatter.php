7564727c1fd45bf8971b3d70820d0e3f10216e64
Benjamin Morel <benjamin.morel@gmail.com>
2017-12-01 10:18:55 +0100

    Formatter for MS (Montserrat)

---
+++ src/Formatter/MSFormatter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Montserrat.
+ *
+ * The format is MSR followed by a space then 4 digits, in the range 1110 to 1350.
+ *
+ * This formatter accepts the postcode both with and without the MSR prefix, and always outputs the prefix.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ */
+class MSFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (substr($postcode, 0, 3) === 'MSR') {
+            $postcode = substr($postcode, 3);
+        }
+
+        if (strlen($postcode) !== 4) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if ($postcode < '1110' || $postcode > '1350') {
+            return null;
+        }
+
+        return 'MSR ' . $postcode;
+    }
+}
---
+++ tests/Formatter/MSFormatterTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\MSFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the MS postcode formatter.
+ */
+class MSFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new MSFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1109', null],
+            ['1110', 'MSR 1110'],
+            ['1111', 'MSR 1111'],
+            ['1234', 'MSR 1234'],
+            ['1349', 'MSR 1349'],
+            ['1350', 'MSR 1350'],
+            ['1351', null],
+            ['12345', null],
+            ['123456', null],
+            ['1234567', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+            ['ABCDEFG', null],
+
+            ['MSR', null],
+            ['MSR1', null],
+            ['MSR12', null],
+            ['MSR123', null],
+            ['MSR1109', null],
+            ['MSR1110', 'MSR 1110'],
+            ['MSR1111', 'MSR 1111'],
+            ['MSR1234', 'MSR 1234'],
+            ['MSR1349', 'MSR 1349'],
+            ['MSR1350', 'MSR 1350'],
+            ['MSR1351', null],
+            ['MSR12345', null],
+            ['XMSR1234', null],
+            ['MSX1234', null],
+        ];
+    }
+}
