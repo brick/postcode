caaf6b38202792372305323b7e0928d366769109
Benjamin Morel <benjamin.morel@gmail.com>
2017-12-01 15:27:50 +0100

    Formatter for SM (San Marino)

---
+++ src/Formatter/SMFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in San Marino.
+ *
+ * The postcode format is 4789N, where N stands for a digit.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ */
+class SMFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if (substr($postcode, 0, 4) !== '4789') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/SMFormatterTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\SMFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the SM postcode formatter.
+ */
+class SMFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new SMFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['47889', null],
+            ['47890', '47890'],
+            ['47891', '47891'],
+            ['47898', '47898'],
+            ['47899', '47899'],
+            ['47900', null],
+            ['123456', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+        ];
+    }
+}
