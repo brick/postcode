f36901b03e3f3618653011946d831dae813832c6
Benjamin Morel <benjamin.morel@gmail.com>
2017-12-01 11:44:01 +0100

    Formatter for NI (Nicaragua)

---
+++ src/Formatter/NIFormatter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Nicaragua.
+ *
+ * Postal codes in Nicaragua are 5 digit numeric.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_Nicaragua
+ */
+class NIFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
