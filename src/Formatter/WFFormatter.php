10b887d079370f8b60afb8e5605b776b91d7b427
Benjamin Morel <benjamin.morel@gmail.com>
2018-01-25 15:23:59 +0100

    Formatter for WF (Wallis and Futuna)

---
+++ src/Formatter/WFFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+
+/**
+ * Validates and formats postcodes in Wallis and Futuna.
+ *
+ * Postcodes consist of 5 digits, without separator.
+ * Overseas Collectivity of France. French codes used. Range 98600 - 98690.
+ *
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ */
+class WFFormatter implements CountryPostcodeFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $postcode) : ?string
+    {
+        if (strlen($postcode) !== 5) {
+            return null;
+        }
+
+        if (! ctype_digit($postcode)) {
+            return null;
+        }
+
+        if ($postcode < '98600' || $postcode > '98690') {
+            return null;
+        }
+
+        return $postcode;
+    }
+}
---
+++ tests/Formatter/WFFormatterTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Postcode\Tests\Formatter;
+
+use Brick\Postcode\CountryPostcodeFormatter;
+use Brick\Postcode\Formatter\WFFormatter;
+use Brick\Postcode\Tests\CountryPostcodeFormatterTest;
+
+/**
+ * Unit tests for the WF postcode formatter.
+ */
+class WFFormatterTest extends CountryPostcodeFormatterTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormatter() : CountryPostcodeFormatter
+    {
+        return new WFFormatter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function providerFormat() : array
+    {
+        return [
+            ['', null],
+
+            ['1', null],
+            ['12', null],
+            ['123', null],
+            ['1234', null],
+            ['12345', null],
+            ['123456', null],
+
+            ['98599', null],
+            ['98600', '98600'],
+            ['98601', '98601'],
+            ['98689', '98689'],
+            ['98690', '98690'],
+            ['98691', null],
+
+            ['A', null],
+            ['AB', null],
+            ['ABC', null],
+            ['ABCD', null],
+            ['ABCDE', null],
+            ['ABCDEF', null],
+        ];
+    }
+}
