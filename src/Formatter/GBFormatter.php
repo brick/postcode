4c985d876b3b5936ff198e473c9f3afe4fdf939a
Benjamin Morel <benjamin.morel@gmail.com>
2020-05-05 15:02:07 +0200

    Restrict the possible letters in GB postcodes

--- src/Formatter/GBFormatter.php
+++ src/Formatter/GBFormatter.php
@@ -19,8 +19,7 @@ use Brick\Postcode\CountryPostcodeFormatter;
  * - AA99 9AA
  *
  * A stands for a capital letter, 9 stands for a digit.
- *
- * The first part is called the outcode, the second part is called the incode.
+ * Only certain letters are allowed for each position in the postcode.
  *
  * @see https://en.wikipedia.org/wiki/List_of_postal_codes
  * @see https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
@@ -28,29 +27,89 @@ use Brick\Postcode\CountryPostcodeFormatter;
 class GBFormatter implements CountryPostcodeFormatter
 {
     /**
-     * The regular expression pattern used to validate the postcode and extract the outcode and incode.
+     * The regular expression patterns, or null if not built yet.
+     *
+     * @var string[]|null
      */
-    private const PATTERN = '/^'
-        . '('
-        . '(?:[A-Z][0-9])|'
-        . '(?:[A-Z][0-9][A-Z])|'
-        . '(?:[A-Z][0-9][0-9])|'
-        . '(?:[A-Z][A-Z][0-9])|'
-        . '(?:[A-Z][A-Z][0-9][A-Z])|'
-        . '(?:[A-Z][A-Z][0-9][0-9])'
-        . ')'
-        . '([0-9][A-Z][A-Z])'
-        . '$/';
+    private $patterns = null;
 
     /**
      * {@inheritdoc}
      */
     public function format(string $postcode) : ?string
     {
-        if (preg_match(self::PATTERN, $postcode, $matches) !== 1) {
-            return null;
+        // regular patterns
+        $patterns = $this->getPatterns();
+
+        foreach ($patterns as $pattern) {
+            if (preg_match($pattern, $postcode, $matches) === 1) {
+                [, $outwardCode, $areaCode, $inwardCode] = $matches;
+
+                // @todo check area code
+
+                return $outwardCode . ' ' . $inwardCode;
+            }
         }
 
-        return $matches[1] . ' ' . $matches[2];
+        return null;
+    }
+
+    /**
+     * Builds the regular expression patterns to check the postcode.
+     *
+     * Each pattern contains 3 capturing groups:
+     *
+     * - The outward code (e.g. WC2E)
+     * - The area code (ex: WC) for additional checks
+     * - The inward code (e.g. 9RZ)
+     *
+     * @return string[]
+     */
+    private function getPatterns() : array
+    {
+        if ($this->patterns !== null) {
+            return $this->patterns;
+        }
+
+        $n = '[0-9]';
+
+        // outward code alpha chars
+        $outAlpha1 = '[ABCDEFGHIJKLMNOPRSTUWYZ]';
+        $outAlpha2 = '[ABCDEFGHKLMNOPQRSTUVWXY]';
+        $outAlpha3 = '[ABCDEFGHJKPSTUW]';
+        $outAlpha4 = '[ABEHMNPRVWXY]';
+
+        // inward code alpha chars
+        $inAlpha = '[ABDEFGHJLNPQRSTUWXYZ]';
+
+        $outPatterns = [];
+
+        // AN
+        $outPatterns[] = '(' . $outAlpha1 . ')' . $n;
+
+        // ANA
+        $outPatterns[] = '(' . $outAlpha1 . ')' . $n . $outAlpha3;
+
+        // ANN
+        $outPatterns[] = '(' . $outAlpha1 . ')' . $n . $n;
+
+        // AAN
+        $outPatterns[] = '(' . $outAlpha1 . $outAlpha2 . ')' . $n;
+
+        // AANA
+        $outPatterns[] = '(' . $outAlpha1 . $outAlpha2 . ')' . $n . $outAlpha4;
+
+        // AANN
+        $outPatterns[] = '(' . $outAlpha1 . $outAlpha2 . ')' . $n . $n;
+
+        $inPattern = $n . $inAlpha . $inAlpha;
+
+        $patterns = [];
+
+        foreach ($outPatterns as $outPattern) {
+            $patterns[] = '/^(' . $outPattern . ')(' . $inPattern . ')$/';
+        }
+
+        return $this->patterns = $patterns;
     }
 }
