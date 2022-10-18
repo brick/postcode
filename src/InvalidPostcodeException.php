e0ccdd15fcee179b523fc6254398d934d802517f
Barney Hanlon <445227+shrikeh@users.noreply.github.com>
2022-10-18 11:11:53 +0100

    Add exception interface for easier catching without having to intimately know the library

--- src/InvalidPostcodeException.php
+++ src/InvalidPostcodeException.php
@@ -7,6 +7,6 @@ namespace Brick\Postcode;
 /**
  * Exception thrown when trying to format an invalid postcode.
  */
-class InvalidPostcodeException extends \Exception
+class InvalidPostcodeException extends \Exception implements PostcodeException
 {
 }
