0013e88f1af910545b0e154f7a88c07736b71873
Alfonso Strotgen <strotgen@gmail.com>
2023-02-12 16:29:18 -0600

    Correctly validate colombian postal codes.

--- src/Formatter/COFormatter.php
+++ src/Formatter/COFormatter.php
@@ -10,25 +10,38 @@ use Brick\Postcode\CountryPostcodeFormatter;
  * Validates and formats postcodes in Colombia.
  *
  * Postal codes in Colombia are 6 digit numeric.
- * The first 2 digits represent the department and can range from 00 to 32.
+ * The first 2 digits represent the department.
  *
  * @see https://en.wikipedia.org/wiki/List_of_postal_codes
- * @see https://en.wikipedia.org/wiki/Postal_codes_in_Colombia
+ * @see https://es.wikipedia.org/wiki/Anexo:C%C3%B3digos_postales_de_Colombia
  */
 class COFormatter implements CountryPostcodeFormatter
 {
+    protected $departments = [
+        '05', '08', '11', '13',
+        '15', '17', '18', '19',
+        '20', '23', '25', '27',
+        '41', '44', '47', '50',
+        '52', '54', '63', '66',
+        '68', '70', '73', '76',
+        '81', '85', '86', '88',
+        '91', '94', '95', '97',
+        '99'
+    ];
+
     /**
      * {@inheritdoc}
      */
-    public function format(string $postcode) : ?string
+    public function format(string $postcode): ?string
     {
-        if (preg_match('/^[0-9]{6}$/', $postcode) !== 1) {
+        if (preg_match('/^\d{2}(?!0000)\d{4}$/', $postcode) !== 1) {
             return null;
         }
 
+
         $department = substr($postcode, 0, 2);
 
-        if ($department > '32') {
+        if (!in_array($department, $this->departments, true)) {
             return null;
         }
 
