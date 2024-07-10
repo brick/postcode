dde115b78df641fa5b630a9e91088b1110130061
rubentebogt <info@webrtb.com>
2024-07-10 15:58:38 +0200

    Enhance exception objects with getters

--- src/UnknownCountryException.php
+++ src/UnknownCountryException.php
@@ -9,4 +9,26 @@ namespace Brick\Postcode;
  */
 class UnknownCountryException extends \Exception
 {
+    protected string $country;
+
+    /**
+     * Construct exception thrown when an unknown country code is provided.
+     *
+     * @param string $country
+     */
+    public function __construct(string $country)
+    {
+        parent::__construct('Unknown country: ' . $country);
+        $this->country = $country;
+    }
+
+    /**
+     * Get the unknown country code associated with this exception.
+     *
+     * @return string
+     */
+    public function getCountryCode(): string
+    {
+        return $this->country;
+    }
 }
