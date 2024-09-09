2807cf2f7ab7bdc092e81b3ed26b17174ff35d3d
Andreas Weswaldi <37552851+weswaldix@users.noreply.github.com>
2024-09-09 14:22:32 +0200

    Fix: Validate Austrian postal codes to exclude invalid values starting with 0
    Previously, postal codes such as '0000' or '0999' were considered valid, but in reality, Austrian postal codes always start with digits from 1 to 9. This update corrects the validation to ensure compliance with the Austrian postal code system (see: https://en.wikipedia.org/wiki/Postal_codes_in_Austria#System).

--- src/Formatter/ATFormatter.php
+++ src/Formatter/ATFormatter.php
@@ -21,7 +21,7 @@ class ATFormatter implements CountryPostcodeFormatter
      */
     public function format(string $postcode) : ?string
     {
-        if (preg_match('/^[0-9]{4}$/', $postcode) !== 1) {
+        if (preg_match('/^[1-9][0-9]{3}$/', $postcode) !== 1) {
             return null;
         }
 
