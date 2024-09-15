5d432125716d98cd9f352183c2e9094c00c4ed6f
Brian Faust <brian@faust.de.com>
2024-09-15 13:16:20 +0300

    Add reference to UPU specification

--- src/Formatter/EGFormatter.php
+++ src/Formatter/EGFormatter.php
@@ -9,10 +9,11 @@ use Brick\Postcode\CountryPostcodeFormatter;
 /**
  * Validates and formats postcodes in Egypt.
  *
- * Postcodes consist of 5 digits, without separator.
+ * Postcodes consist of 5 or 7 digits, without separator.
  *
  * @see https://en.wikipedia.org/wiki/List_of_postal_codes
  * @see https://en.wikipedia.org/wiki/List_of_postal_codes_in_Egypt
+ * @see https://www.upu.int/UPU/media/upu/PostalEntitiesFiles/addressingUnit/egyEn.pdf
  */
 class EGFormatter implements CountryPostcodeFormatter
 {
