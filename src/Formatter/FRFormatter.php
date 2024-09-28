7c11f827b24778d581798b343ab91f819e2f589f
Johnny X. Lemonade <johnnyxlemonade@users.noreply.github.com>
2024-09-28 09:46:28 +0200

    Add stricter regex for validating French postal codes
    Updated the postal code validation regex to enforce the specific structure of French postal codes. The new regex ensures that the first two digits fall within the valid ranges (01–95 for metropolitan areas and 96–98 for overseas territories), while still allowing any valid 5-digit code. This improves accuracy over the previous general 5-digit validation, which allowed invalid codes.

--- src/Formatter/FRFormatter.php
+++ src/Formatter/FRFormatter.php
@@ -11,13 +11,20 @@ use Brick\Postcode\CountryPostcodeFormatter;
  *
  * Postcodes consist of 5 digits, without separator.
  *
+ * First two digits indicate the department (region)
+ * 01–95 for metropolitan areas
+ * 96–98 for overseas territories
+ *
  * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @see https://en.wikipedia.org/wiki/Postal_codes_in_France
  */
 class FRFormatter implements CountryPostcodeFormatter
 {
+
     public function format(string $postcode) : ?string
     {
-        if (preg_match('/^[0-9]{5}$/', $postcode) !== 1) {
+
+        if (preg_match('/^(?:0[1-9]|[1-8]\d|9[0-8])\d{3}$/', $postcode) !== 1) {
             return null;
         }
 
